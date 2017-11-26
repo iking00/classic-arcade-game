24d21638f0eed7ed6ebd1342c714b7af1b55ddb4
Ian King <ian_king@comcast.net>
2017-11-26 09:45:54 -0500

    feat: set enemy update method to give enemy movement

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    const x = this.x + this.moveSpeed * dt;
+    x < 606 ? this.x = x : this.x = -101;
 };
 
 // Draw the enemy on the screen, required method for game
