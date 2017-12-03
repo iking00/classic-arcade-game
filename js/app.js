8cebb6a42fe09f3800235200ff220df0daacdda8
Ian King <ian_king@comcast.net>
2017-12-03 12:08:28 -0500

    docs: updates app.js documentation

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ class Entity {
         this.yOffset = yOffset;
     }
 
+    //draw image on convas
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
@@ -24,14 +25,19 @@ class Entity {
 * @constructor
 * @param {number} row - the row position for enemy
 * @param {integer} moveSpeed - the amount to increment x coordinate for enemy movement
+* @param {integer} x - x coordinate on canvas; use default to have it start off canvas
+* @param {integer} yOffset - amount to offset y coordinate to properly position entity
+* @param {string} sprite - image file representing entity
 */
 class Enemy extends Entity {
     constructor(row, moveSpeed, x = -101, yOffset = 23, sprite = 'images/enemy-bug.png'){
+        //based on row parameter move starting y position by height of blocks then offset to properly position entity
         const y = row * 83 - yOffset;
         super(sprite, x, y, yOffset);
         this.moveSpeed = moveSpeed;
     }
 
+    //modify the x,y to draw image and check for collision
     update(dt){
         const x = this.x + this.moveSpeed * dt;
         //if movement pushes the enemy outside canvas then reset it
@@ -43,7 +49,7 @@ class Enemy extends Entity {
         //get position of enemy
         const enemyLeft = this.x;
         const enemyRight = enemyLeft + 101;
-        //if enemy and player are on the same y position and the enemy is intersecting the player
+        //if enemy and player are on the same y position and the enemy is intersecting the player then start over
         if(enemyRight >= playerLeft && enemyLeft <= playerRight && this.y === player.y) {
             player.collision(); 
             resetStars();
@@ -54,6 +60,12 @@ class Enemy extends Entity {
 /**
 * @description Represents a Player
 * @constructor
+* @param {integer} row - the starting row position for player
+* @param {integer} col - the starting column position for player
+* @param {integer} yOffset - the amount to offset y coordinate to ensure proper position
+* @param {string} sprite - the image to draw for player
+* @param {integer} x - the x coordinate on canvas; will be set by update method
+* @param {integer} y - the y coordinate on canvas; will be set by update method
 */
 class Player extends Entity {
     constructor(row = 5, col = 2, yOffset = 23, sprite = 'images/char-boy.png', x = 0, y = 0){
@@ -63,12 +75,14 @@ class Player extends Entity {
     }
 
     update(){
+        //based on column parameter set x position multiplied by width of blocks
         this.x = this.col * 101;
+        //based on row parameter set y position multilied by height of blocks then offset to properly position entity
         this.y = this.row * 83 - this.yOffset;
     }
 
+    //based on key pressed; increment or decrement column/row which will later set x/y position in update method; do nothing if move is outside canvas
     handleInput(keyCode){
-        //based on key pressed; increment or decrement column/row; do nothing if move is outside canvas
         switch(keyCode){
             case 'left':
                 if(this.col > 0) {this.col -= 1;}
@@ -87,6 +101,7 @@ class Player extends Entity {
         }
     }
 
+    //player made it to top; reset player and star display position; add win count
     won(){
         this.reset();
         winCount.increment();
@@ -94,27 +109,41 @@ class Player extends Entity {
         resetStars();
     }
 
+    //player collided with enemy; reset player position; increment collision count
     collision(){
         this.reset();
         collisionCount.increment();
         collisionCount.display();
     }
 
+    //reset player to starting column/row which will later set x/y position in update method
     reset(){
         this.row = 5;
         this.col = 2;
     }
 }
 
+
+/**
+* @description Represents a Star
+* @constructor
+* @param {integer} row - the starting row position for Star
+* @param {integer} col - the starting column position for Star
+* @param {string} sprite - the image to draw for star
+* @param {integer} yOffset - the amount to offset y coordinate to ensure proper position
+*/
 class Star extends Entity {
     constructor(row, col, sprite = 'images/star.png', yOffset = 10){
+        //based on column parameter set x position multiplied by width of blocks
         const x = col * 101;
+        //based on row parameter set y position multilied by height of blocks then offset to properly position entity
         const y = row * 83 - yOffset;
         super(sprite, x, y, yOffset);
         this.row = row;
         this.col = col;
     }
 
+    //determine if player intersects star
     update(){
         const playerCol = player.col;
         const playerRow = player.row;
@@ -126,6 +155,7 @@ class Star extends Entity {
         }
     }
 
+    //redisplay the star on screen
     reset(){
         const x = this.col * 101;
         const y = this.row * 83 - this.yOffset;
@@ -135,7 +165,7 @@ class Star extends Entity {
 }
 
 /**
-* @description: reset diplay of stars so those drawn off canas are shown again
+* @description: reset diplay of stars so those drawn off canvas are shown again
 */
 function resetStars() {
     for (const star of allStars){
@@ -211,10 +241,10 @@ function resetGame(){
     starCount.reset();
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// store all enemy objects in allEnemies array
 let allEnemies = [];
+// array of arrays to store initial configuration of enemies [0] is the row position [1] is the move speed
+// update this array to change row position and move speed of enemies
 const enemies = [
     [1, 80],
     [1, 400],
@@ -226,9 +256,13 @@ const enemies = [
 for (const enemy of enemies) {
     allEnemies.push(new Enemy(enemy[0],enemy[1]));
 }
+//initiate player
 const player = new Player();
 
+// store all star objects in allStars array
 let allStars = [];
+// array of arrays to store initial configuration of stars [0] is the row position [1] is the column position
+// update this array to change row position and move speed of enemies
 const stars = [
     [1,2],
     [2,0],
@@ -237,6 +271,7 @@ const stars = [
 for (const star of stars){
     allStars.push(new Star(star[0],star[1]));
 }
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -251,7 +286,7 @@ document.addEventListener('keyup', function(e) {
 });
 
 /**
-* @description On character selection update the character image and render on screen
+* @description - On character selection update the character image and render on screen
 */
 function playerSelected(){
     player.sprite = 'images/' + $(this).val();
